35160e192f1256cb5fd27ce73f0df9e25b3c75fa
Vartok Soma <vartok.soma@gmail.com>
2019-03-07 14:14:47 +0100

    set up AsynTask for downloading image

--- app/src/main/java/com/somavartok/downloadimage/MainActivity.java
+++ app/src/main/java/com/somavartok/downloadimage/MainActivity.java
@@ -1,21 +1,66 @@
 package com.somavartok.downloadimage;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView downloadedImage;
 
     public void downloadImage(View view){
 
+        //https://upload.wikimedia.org/wikipedia/commons/8/82/Husqvarna_Vitpilen_701.jpg
+        ImageDownloader task = new ImageDownloader();
+        Bitmap myImage;
+        try {
+            myImage = task.execute("https://upload.wikimedia.org/wikipedia/commons/8/82/Husqvarna_Vitpilen_701.jpg").get();
+            downloadedImage.setImageBitmap(myImage);
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         Log.i("Interaction", "Button has been tapped");
 
+    }
+
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap>{
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.connect();
+                InputStream stream = urlConnection.getInputStream();
+                Bitmap image = BitmapFactory.decodeStream(stream);
+                return image;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         downloadedImage = findViewById(R.id.imageView);
     }
 }
